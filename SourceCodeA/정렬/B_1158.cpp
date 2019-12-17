5a1d931095c74a3e3ac3ede5cb850f2ddbaec93d
SimpleisNice <40995819+SimpleisNice@users.noreply.github.com>
2019-12-17 13:20:56 +0900

    Solve Baekjoon 1158
    - 1158 조세퍼스

--- SourceCodeA/정렬/B_1158.cpp
+++ SourceCodeA/정렬/B_1158.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+struct JO_LIST
+{
+    int num;
+    bool isCheck;
+};
+
+struct JO_LIST tempList[5001];
+vector<int> tempVector;
 
 int main(void)
 {
+    cin.tie(0);
+    ios_base::sync_with_stdio(false);
     int n,k;
     cin >> n >> k;
-    vector<int> tempQ(n);
 
-    for(int i = 0; i < n; ++i)
-    {
-        tempQ[i] = i + 1;
-    }
-
-    for(auto it = tempQ.begin(); it != tempQ.end(); ++it)
-    {
-        cout << *it << endl;
-    }
+    tempVector.resize(n);
     
-    int popIndex = 0;
-    while(!tempQ.empty())
+    for(int i=1; i<=n; ++i)
     {
-        if(popIndex + k )
+        tempList[i].num = i;
+        tempList[i].isCheck = false;
     }
+
+    int useCount = 0;
+    int index = 0;
+
+    while(true)
+    {
+        int moveCheck = 0;
+        if(useCount == n)
+            break;
+
+        while(true)
+        {
+            if(moveCheck == k)
+            {   
+                tempList[index].isCheck = true;
+                tempVector[useCount] = tempList[index].num;
+                ++useCount;
+                break;
+            }
+            if (index + 1 > n)
+                index = 1;
+            else 
+                ++index;
+
+            if(tempList[index].isCheck)
+                continue;
+            ++moveCheck;
+        }
+    }
+
+    cout << '<';
+    for(int i=0; i<n-1; ++i)
+    {
+        cout << tempVector[i] << ", ";
+    }
+    cout << tempVector[n-1] << ">\n";
+
     return 0;
 }

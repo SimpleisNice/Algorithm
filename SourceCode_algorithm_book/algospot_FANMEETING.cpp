daffd57c12e0ca615c345f07d9dc14d6614c4e48
SimpleisNice <40995819+SimpleisNice@users.noreply.github.com>
2020-04-13 23:48:21 +0900

    Solve algorithm(algospot)
    FANMEETING

---
+++ SourceCode_algorithm_book/algospot_FANMEETING.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    int c;
+    cin >> c;
+    
+    while(c--) {
+        string members, fans;
+        cin >> members >> fens;
+        int membersCnt = members.length();
+        int fensCnt = members.length();
+        
+    }
+}

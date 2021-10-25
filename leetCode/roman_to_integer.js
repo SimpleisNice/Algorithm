38e0bfd6b9bdd1e1f6c8cf62e1c762556f282900
SimpleisNice <cgh3407@gmail.com>
2021-10-26 00:16:43 +0900

    Algorithm: roman_to_integer * 문제는 해결했지만, 더 좋은 방법 적용 필요

--- leetCode/roman_to_integer.js
+++ leetCode/roman_to_integer.js
@@ -1,3 +1,57 @@
 /**
  * https://leetcode.com/problems/roman-to-integer/
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var romanToInt = function(s) {  
+  const SPECIAL_TYPE = {
+      IV: 4,
+      IX: 9,
+      XL: 40,
+      XC: 90,
+      CD: 400,
+      CM: 900,
+  }
+  const NORMAL_TYEP = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+  };
+  
+  let totalCount = 0;
+  let tempString = s;
+  
+  Object.keys(SPECIAL_TYPE).forEach(value => {
+      let count = 0;
+      let index = tempString.indexOf(value);
+      
+      while(index !== -1) {
+          count += 1;
+          tempString = tempString.replace(value, ' ');
+          index = tempString.indexOf(value);
+      }
+      
+      totalCount += count * SPECIAL_TYPE[value];
+  });
+  
+  Object.keys(NORMAL_TYEP).forEach(value => {
+      let count = 0;
+      let index = tempString.indexOf(value);
+      
+      while(index !== -1) {
+          count += 1;
+          tempString = tempString.replace(value, ' ');
+          index = tempString.indexOf(value);
+      }
+      
+      totalCount += count * NORMAL_TYEP[value];
+  });
+  
+  return totalCount;
+};
